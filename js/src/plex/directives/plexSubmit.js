8b13850d1180a98a3d475c4bd2aadc6d4a517b97
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-04-11 14:01:11 -0300

    remover dependencia de angular-material en bower

--- js/src/plex/directives/plexSubmit.js
+++ js/src/plex/directives/plexSubmit.js
@@ -6,18 +6,43 @@
  * Emite un evento ```$plex-before-submit``` para indicar a la directiva {@link module:plex.directive:plex} que valide el modelo asociado.
  * @restrict A
  **/
-angular.module('plex').directive("plexSubmit", ["$parse", function ($parse) {
+angular.module('plex').directive("plexSubmit", ["$parse", "$timeout", function($parse, $timeout) {
     return {
         restrict: "A",
         require: '^form',
         scope: false,
-        link: function (scope, element, attrs, formController) {
+        link: function(scope, element, attrs, formController) {
             var fn = $parse(attrs.plexSubmit);
-            element.on('click', function (event) {
-                scope.$apply(function () {
+            element.on('click', function(event) {
+                scope.$apply(function() {
                     scope.$broadcast('$plex-before-submit', formController);
-                    if (formController.$valid)
-                        fn(scope, { $event: event });
+                    if (formController.$valid) {
+                        var promise = fn(scope, {
+                            $event: event
+                        });
+                        if (promise && promise.finally) {
+                            // Disable button
+                            element.attr('disabled', 'disabled');
+                            // DOM changes
+                            var icon = element.children("I").eq(0);
+                            var originalClass = icon[0].className;
+                            icon.removeClass();
+                            icon.addClass('mdi mdi-google-circles-extended icon-spinner');
+                            // When done ...
+                            promise.finally(function() {
+                                // Restore button state
+                                element.removeAttr('disabled');
+                                // Show check icon
+                                icon.removeClass();
+                                icon.addClass('mdi mdi-check');
+                                // Restore original icon
+                                $timeout(function(){
+                                    icon.removeClass();
+                                    icon.addClass(originalClass);
+                                }, 2000);
+                            });
+                        }
+                    }
                 });
             });
         }
